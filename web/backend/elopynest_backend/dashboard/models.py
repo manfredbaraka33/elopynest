9981c0ee0f3eae61e3a9886600f39cbdfa5315b8
Manfred Baraka Josephat <163660029+manfredbaraka33@users.noreply.github.com>
2025-10-05 09:48:29 +0300

    Update models.py

--- web/backend/elopynest_backend/dashboard/models.py
+++ web/backend/elopynest_backend/dashboard/models.py
@@ -147,7 +147,7 @@ class MoodLog(models.Model):
 
 class DeviceToken(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    token = models.CharField(max_length=255, unique=True)
+    token = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     
     
