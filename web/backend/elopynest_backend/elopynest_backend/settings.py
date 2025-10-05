db8ee93ae8ff69f94cae6f74b1683e4514201376
Manfred Baraka Josephat <163660029+manfredbaraka33@users.noreply.github.com>
2025-10-05 07:35:51 +0300

    Update settings.py

--- web/backend/elopynest_backend/elopynest_backend/settings.py
+++ web/backend/elopynest_backend/elopynest_backend/settings.py
@@ -122,7 +122,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv("DB_NAME"),
-        'USER': os.getenv("DB_USER),
+        'USER': os.getenv("DB_USER"),
         'PASSWORD': os.getenv("DB_PASSWORD"),
         'HOST': os.getenv("DB_URL"),
         'PORT': '5432',
